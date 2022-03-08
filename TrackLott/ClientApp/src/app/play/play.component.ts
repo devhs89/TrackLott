47d68b5b6fe36c9ccf8cd923510b7372a711055b
Harpreet Singh <developer.hs89@gmail.com>
2022-03-09 03:12:31 +1100

    PROGRESS: from zenbook

--- TrackLott/ClientApp/src/app/play/play.component.ts
+++ TrackLott/ClientApp/src/app/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-play',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play.component.scss']
 })
 export class PlayComponent implements OnInit {
+  deltaNums: number[][] = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5, 6, 7],
+    [7, 9],
+    [8, 9, 10, 11, 12, 13, 14, 15]
+  ];
+  rowOnePickedNum: number = 0;
+  rowTwoPickedNums: number[] = [];
+  rowThreePickedNum: number = 0;
+  rowFourPickedNums: number[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onClickBtn(event: MouseEvent, outerDex: number) {
+    // @ts-ignore
+    const btnSelected: number = +event.target?.textContent;
+
+    switch (outerDex) {
+      case 0:
+        this.rowOnePickedNum === btnSelected ? this.rowOnePickedNum = 0 : this.rowOnePickedNum = btnSelected;
+        break;
+      case 1:
+        this.rowTwoPickedNums.includes(btnSelected)
+          ? this.rowTwoPickedNums.splice(this.rowTwoPickedNums.findIndex(item => item === btnSelected), 1)
+          : this.rowTwoPickedNums.push(btnSelected);
+        break;
+    }
+  }
 }
