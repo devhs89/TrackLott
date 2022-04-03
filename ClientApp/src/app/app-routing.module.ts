5668e0c6d82b39679cf66952c8e701c8b8061f72
Harpreet Singh <developer.hs89@gmail.com>
2022-04-03 15:47:53 +1000

    FEATURE: add terms and conditions

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {AddComponent} from "./add/add.component";
 import {PlayComponent} from "./play/play.component";
 import {AuthGuardService} from "./services/auth-guard.service";
 import {LicenseComponent} from "./license/license.component";
+import {TermsComponent} from "./terms/terms.component";
 
 const routes = [
   {path: 'home', component: HomeComponent},
   {path: 'add', component: AddComponent, canActivate: [AuthGuardService]},
   {path: 'play', component: PlayComponent},
   {path: 'license', component: LicenseComponent},
+  {path: 'terms', component: TermsComponent},
   {
     path: 'user',
     loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)
