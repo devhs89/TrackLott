6914461d2bf34490c99733a851188f43b4f06edc
Harpreet Singh <developer.hs89@gmail.com>
2022-04-16 22:09:39 +1000

    FIX: get rid of 2 sec delay added whilst testing *sigh

--- TrackLott/ClientApp/src/app/interceptors/loading.interceptor.ts
+++ TrackLott/ClientApp/src/app/interceptors/loading.interceptor.ts
@@ -1,6 +1,6 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {ProgressIndicatorService} from "../services/progress-indicator.service";
-import {delay, finalize, Observable} from "rxjs";
+import {finalize, Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 
 @Injectable({
@@ -15,7 +15,6 @@ export class LoadingInterceptor implements HttpInterceptor {
     this.loadingService.show();
 
     return httpHandler.handle(httpRequest).pipe(
-      delay(2000),
       finalize(() => this.loadingService.hide())
     );
   }
