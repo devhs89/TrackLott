1057f9a78d3c9aca6551788312daf344a2a11774
Harpreet Singh <developer.hs89@gmail.com>
2022-06-08 15:01:07 +1000

    FIX: replace new line characters with html breaks in login and register email notifications

--- TrackLott/Services/MailNoticeService.cs
+++ TrackLott/Services/MailNoticeService.cs
@@ -15,14 +15,14 @@ public class MailNoticeService : IMailNoticeService
   public async Task<string> RegisterNotification(AppUser appUser)
   {
     var resp = await PerformCall("New Registration",
-      $"New User Registration\n\nUser: {appUser.GivenName} {appUser.Surname} <{appUser.Email}>\n\nSelected Country: {appUser.Country}\n\nTerms Accepted: {appUser.TermsCheck}");
+      $"New User Registration<br /><br />User: {appUser.GivenName} {appUser.Surname} <{appUser.Email}><br />Selected Country: {appUser.Country}<br /><br />Terms Accepted: {appUser.TermsCheck}");
     return resp;
   }
 
   public async Task<string> LoginNotification(AppUser appUser, bool attemptStatus)
   {
     var resp = await PerformCall("Login Attempt",
-      $"Login Attempt\n\nUser: {appUser.GivenName} {appUser.Surname} <{appUser.Email}>\n\nSelected Country: {appUser.Country}\n\nSuccessful: {attemptStatus}");
+      $"Login Attempt<br /><br />User: {appUser.GivenName} {appUser.Surname} <{appUser.Email}><br />Selected Country: {appUser.Country}<br /><br />Successful: {attemptStatus}");
     return resp;
   }
 
