ae29c264cbd20a6e4742f6223b5cf5f1be110299
Harpreet Singh <developer.hs89@gmail.com>
2022-06-21 12:13:11 +1000

    FEATURE: create token with env dependent issuer and audience

--- TrackLott/Services/TokenService.cs
+++ TrackLott/Services/TokenService.cs
@@ -11,10 +11,12 @@ namespace TrackLott.Services;
 public class TokenService
 {
   private readonly UserManager<UserModel> _userManager;
+  private readonly IWebHostEnvironment _env;
 
-  public TokenService(UserManager<UserModel> userManager)
+  public TokenService(UserManager<UserModel> userManager, IWebHostEnvironment env)
   {
     _userManager = userManager;
+    _env = env;
   }
 
   public async Task<string> CreateToken(UserModel userModel)
@@ -29,8 +31,14 @@ public class TokenService
     var rsa = await CryptoSystem.GetPrivateRsaKey();
     var credentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
 
-    var jwtSecurityToken = new JwtSecurityToken(new JwtHeader(credentials),
-      new JwtPayload("localhost", "localhost", claims, DateTime.Now, DateTime.Now.AddDays(7)));
+    var jwtSecurityToken = new JwtSecurityToken(
+      new JwtHeader(credentials),
+      new JwtPayload(
+        _env.IsProduction() ? DomainName.TrackLottUsualAppsCom : DomainName.Localhost8001,
+        _env.IsProduction() ? DomainName.TrackLottUsualAppsCom : DomainName.Localhost8001,
+        claims,
+        DateTime.Now,
+        DateTime.Now.AddDays(7)));
 
     var writtenToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
     if (writtenToken == null) throw new SecurityTokenException(ErrorResponse.UnableToWriteToken);
