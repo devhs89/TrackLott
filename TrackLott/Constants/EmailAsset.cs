31478d017eea94b41a3523624b9da3599a9680bc
Harpreet Singh <developer.hs89@gmail.com>
2022-07-08 02:24:24 +1000

    REFACTOR: remove redundant code from back-end constants

--- TrackLott/Constants/EmailAsset.cs
+++ TrackLott/Constants/EmailAsset.cs
@@ -4,6 +4,5 @@ public static class EmailAsset
 {
   public const string NoReplyServerEmail = "no-reply@usualapps.com";
   public const string NoReplyProjectName = "TrackLott (No Reply)";
-  public const string ServerAdminEmail = "webmail.hs89@gmail.com";
   public const string ServerAdminName = "Harpreet Singh";
 }
