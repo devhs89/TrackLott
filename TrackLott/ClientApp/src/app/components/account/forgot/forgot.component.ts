c74bf04d2e0b9f3e36fd5215ce1e47b07ec98c7b
Harpreet Singh <developer.hs89@gmail.com>
2022-07-11 14:40:11 +1000

    FEATURE: use mat-card in forgot template

--- TrackLott/ClientApp/src/app/components/account/forgot/forgot.component.ts
+++ TrackLott/ClientApp/src/app/components/account/forgot/forgot.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {genericConst} from "../../../constants/generic-const";
+import {appRoute} from "../../../constants/app-route";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.component.html',
   styleUrls: ['./forgot.component.scss']
 })
-export class ForgotComponent implements OnInit {
+export class ForgotComponent {
+  cardClasses = genericConst.cardClasses;
+  appRoute = appRoute;
 
-  constructor() {
+  onForgotSubmit(forgotForm: NgForm) {
+    if (forgotForm.invalid) return;
+    console.log(forgotForm);
   }
-
-  ngOnInit(): void {
-  }
-
 }
