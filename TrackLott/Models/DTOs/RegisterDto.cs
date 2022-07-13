0dae930a324fd26c32acd3d11383730813691fae
Harpreet Singh <developer.hs89@gmail.com>
2022-07-13 13:17:39 +1000

    FIX: perform data validation at registerDTO rather than at model

--- TrackLott/Models/DTOs/RegisterDto.cs
+++ TrackLott/Models/DTOs/RegisterDto.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TrackLott.Models.DTOs;
 
 public class RegisterDto
 {
-  public string Email { get; set; }
+  [Required, EmailAddress] public string Email { get; set; }
+
+  [Required, StringLength(100, MinimumLength = 8)]
   public string Password { get; set; }
-  public string RepeatPassword { get; set; }
-  public string GivenName { get; set; }
-  public string Surname { get; set; }
+
+  [Required] public string RepeatPassword { get; set; }
+  [Required] public string GivenName { get; set; }
+  [Required] public string Surname { get; set; }
+
+  [Required, RegularExpression("\\d\\d\\d\\d-\\d\\d-\\d\\d")]
   public string Dob { get; set; }
-  public string Country { get; set; }
-  public bool TermsCheck { get; set; }
+
+  [Required] public string Country { get; set; }
+  [Required] public bool TermsCheck { get; set; }
 }
--- TrackLott/Models/DataModels/TrackLottUserModel.cs
+++ TrackLott/Models/DataModels/TrackLottUserModel.cs
@@ -1,14 +1,13 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace TrackLott.Models.DataModels;
 
 public class TrackLottUserModel : IdentityUser<Guid>
 {
-  [Required] public string GivenName { set; get; }
-  [Required] public string Surname { get; set; }
-  [Required] public DateOnly Dob { get; set; }
-  [Required] public string Country { get; set; }
-  [Required] public bool TermsCheck { get; set; }
+  public string GivenName { set; get; }
+  public string Surname { get; set; }
+  public DateOnly Dob { get; set; }
+  public string Country { get; set; }
+  public bool TermsCheck { get; set; }
   public ICollection<CombinationModel> Combinations { get; set; }
 }
