da74fee5a1791cb68e0cd03a5b97dbe0c97323f2
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2022-12-02 21:54:03 -0500

    REFACTOR: cleanup HelperServicesExtension

--- TrackLott/Extensions/HelperServicesExtension.cs
+++ TrackLott/Extensions/HelperServicesExtension.cs
@@ -4,9 +4,8 @@ namespace TrackLott.Extensions;
 
 public static class HelperServicesExtension
 {
-  public static IServiceCollection AddHelperServices(this IServiceCollection serviceCollection)
+  public static void AddHelperServices(this IServiceCollection serviceCollection)
   {
     serviceCollection.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-    return serviceCollection;
   }
 }
